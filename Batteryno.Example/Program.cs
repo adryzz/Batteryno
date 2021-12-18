5a78b3763d96c30460a955497d193fed8f9aaee1
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-18 21:14:52 +0100

    test

--- Batteryno.Example/Program.cs
+++ Batteryno.Example/Program.cs
@@ -24,6 +24,16 @@ namespace Batteryno.Example
                     Console.WriteLine($"Battery energy when full: {batt.FullEnergy} uWh");
                     Console.WriteLine($"Battery energy when full and new: {batt.FullDesignEnergy} uWh");
                     Console.WriteLine($"Battery energy: {batt.Energy} uWh");
+                    Console.WriteLine($"Battery voltage: {batt.Voltage} uV");
+                    Console.WriteLine($"Battery minimum voltage: {batt.MinimumVoltage} uV");
+                    Console.WriteLine($"Charge cycles: {batt.ChargeCycles}");
+                    Console.WriteLine($"Status: {batt.Status}");
+                    Console.WriteLine($"Battery type: {batt.Type}");
+                    Console.WriteLine($"Battery technology: {batt.Technology}");
+                    Console.WriteLine($"Manufacturer: {batt.Manufacturer}");
+                    Console.WriteLine($"Model name: {batt.ModelName}");
+                    Console.WriteLine($"Serial number: {batt.SerialNumber}");
+                    Console.WriteLine($"Is present: {batt.Present}");
                 }
             }
         }
