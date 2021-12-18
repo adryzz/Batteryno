6a3d60d67e69df46a9254407ebc6cfdc2c7e2f32
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-18 19:48:37 +0100

    yep

--- Batteryno/Power.cs
+++ Batteryno/Power.cs
@@ -11,7 +11,7 @@ namespace Batteryno
         public static IEnumerable<Battery> GetConnectedBatteries()
         {
             List<Battery> batteries = new List<Battery>();
-            IEnumerable<string> bats = Directory.EnumerateDirectories(CLASS_PATH).Where(x => x.StartsWith("BAT"));
+            IEnumerable<string> bats = Directory.EnumerateDirectories(CLASS_PATH, "BAT*");
             Console.WriteLine($"Found {bats.Count()} batteries");
             Console.ReadLine();
             return batteries;
