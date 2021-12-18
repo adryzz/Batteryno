dc0f96261b11c1196d229e1f930bfedcb1a1efd0
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-18 20:13:48 +0100

    add energy

--- Batteryno/Battery.cs
+++ Batteryno/Battery.cs
@@ -11,13 +11,34 @@ public class Battery
     public string BatteryPath { get; }
     
     /// <summary>
-    /// Battery capacity (percentage)
+    /// The battery capacity (percentage)
     /// </summary>
     public int Capacity => _capacity;
     private int _capacity;
 
+    /// <summary>
+    /// The battery capacity level
+    /// </summary>
     public CapacityLevel CapacityLevel => _capacityLevel;
     private CapacityLevel _capacityLevel;
+
+    /// <summary>
+    /// The battery energy when full (uWh)
+    /// </summary>
+    public long FullEnergy => _fullEnergy;
+    private long _fullEnergy;
+    
+    /// <summary>
+    /// The battery energy when full and new (uWh)
+    /// </summary>
+    public long FullDesignEnergy => _fullDesignEnergy;
+    private long _fullDesignEnergy;
+    
+    /// <summary>
+    /// The battery energy (uWh)
+    /// </summary>
+    public long Energy => Energy;
+    private long _energy;
     
     internal Battery(string path)
     {
@@ -38,5 +59,8 @@ public class Battery
     {
         _capacity = int.Parse(File.ReadAllText(Path.Combine(BatteryPath, "capacity")));
         _capacityLevel = Enum.Parse<CapacityLevel>(File.ReadAllText(Path.Combine(BatteryPath, "capacity_level")));
+        _fullEnergy = long.Parse(File.ReadAllText(Path.Combine(BatteryPath, "energy_full")));
+        _fullDesignEnergy = long.Parse(File.ReadAllText(Path.Combine(BatteryPath, "energy_full_design")));
+        _energy = long.Parse(File.ReadAllText(Path.Combine(BatteryPath, "energy_now")));
     }
 }
