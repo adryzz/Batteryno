4a1bc3194c1d64ca0333730382a4529a02674eda
Adryzz <46694241+adryzz@users.noreply.github.com>
2021-12-18 19:38:58 +0100

    startup project

---
+++ Batteryno.Example/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Batteryno;
+
+namespace Batteryno.Example
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Power.GetConnectedBatteries();
+        }
+    }
+}
--- Batteryno/Power.cs
+++ Batteryno/Power.cs
@@ -1,7 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 namespace Batteryno
 {
-    public class Power
+    public static class Power
     {
-        
+        private const string CLASS_PATH = "/sys/class/power_supply/";
+        public static IEnumerable<Battery> GetConnectedBatteries()
+        {
+            List<Battery> batteries = new List<Battery>();
+            IEnumerable<string> bats = Directory.EnumerateDirectories(CLASS_PATH).Where(x => x.StartsWith("BAT"));
+            Console.WriteLine($"Found {bats.Count()} batteries");
+            return batteries;
+        }
     }
 }
